8d33cf6e949813e721c549457ef097052a98aa9f
felipeverger <felipeverger123@gmail.com>
2022-05-10 19:20:15 -0300

    first commit

---
+++ utils/fetchApi.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const baseUrl = "https://bayut.p.rapidapi.com"
+
+
+
+export const fetchApi = async (url) => {
+    const { data } = await axios.get((url), {
+        headers: {
+            'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
+            'X-RapidAPI-Key': '458f275680mshc8ace05334acccfp1de458jsne62ddfcc6c14'
+        }
+    })
+
+    return data;
+}
